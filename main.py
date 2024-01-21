a87881e323fc33989179536d34bfa491f0e4c2f5
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2024-01-21 19:53:56 +0430

    Day 3 Practice

--- main.py
+++ main.py
@@ -1,55 +1,25 @@
-# # Data Types
-# print("Hello"[4])
-# print("123" + "456")
-# # Integer
-# print(123 + 456)
-# print(123_456_7890)
-# # Float
-# print(3.14159)
-# # Boolean
-# print(True)
-# print(False)
-# num_char = len(input("What is your name? "))
-# print("Your name has " + str(num_char) + " characters.")
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+if height >= 120:
+    print("You can ride rollercoaster!")
+    age = int(input("What is your age? "))
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7.")
+    elif age >= 45 and age <= 65:
+        print("Everything is going to br ok, Have a free ride on us!")
+    else:
+        bill = 12
+        print("Adult tickets are $12.")
 
-# a = float(123)
-# print(type(a))
-# print(70 + float("100.5"))
-# print(str(70)+str(100.5))
+    want_photo = input("Do you want a photo taken? Y or N. ")
+    if (want_photo.upper() == "Y"):
+        bill += 3
 
-# 3 + 5
-# 7 - 4
-# 3 * 2
-# 6 / 2
-# 2 ** 3
-
-# PEMDAS
-# Parentheses -> ()
-# Exponents -> **
-# Multiplication -> * or Division -> /
-# Addition -> + or Subtraction -> -
-
-# print(3 * (3 + 3) / 3 - 3)
-
-print(8 / 3)
-print(int(8 / 3))
-print(round(8 / 3, 2))
-
-# Floor division
-print(8 // 3)
-# class int
-print(type(8 // 3))
-# class float
-print(type(8 / 3))
-
-result = 16 / 4
-result /= 2
-print(result)
-
-# f-String
-score = 3
-height = 1.8
-isWinning = True
-print("Your score is " + str(score) + ", your height is " + str(height) + ", you are winning " + str(isWinning))
-# or
-print(f"Your score is {score}, your height is {height}, you are winning {isWinning}")
+    print(f"Your final Bill is ${bill}.")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
