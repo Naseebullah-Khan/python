8929964434d843732807c1277900c0d34be6e117
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2024-01-21 20:07:45 +0430

    Day 5 Practice

--- main.py
+++ main.py
@@ -1,31 +1,13 @@
-# import random
-# import my_module
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#   print(fruit)
+#   print(fruit + " Pie")
 
-# random_integer = random.randint(1, 100)
+# # For loop with rang function
+# for number in range(1, 11, 3):
+#   print(number)
 
-# print(random_integer)
-
-# print(my_module.pi)
-
-# random_float = random.random() * 5
-# print(random_float)
-
-# province_of_afghanistan = ["Kabul", "Kandahar", "Mazar", "Herat", "Jalalabad"]
-
-# print(province_of_afghanistan)
-# province_of_afghanistan[1] = "Qandahar"
-# print(province_of_afghanistan)
-
-# province_of_afghanistan.append("Kunar")
-# print(province_of_afghanistan)
-
-# province_of_afghanistan.extend(["Bameyan", "Laghman", "Logar"])
-# print(province_of_afghanistan)
-
-dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
-
-fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
-vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
-
-dirty_dozen = [fruits, vegetables]
-print(dirty_dozen)
+sum = 0
+for number in range(1, 101):
+  sum += number
+print(sum)
