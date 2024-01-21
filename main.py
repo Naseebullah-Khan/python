ff8468272f558cef53f21651a7d441dd4519b552
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2024-01-21 18:51:17 +0430

    Day 1 Practice

---
+++ main.py
@@ -0,0 +1,28 @@
+# Write your code below this line 👇
+# print("Hello, World!")
+# input("What is your name?")
+# print("Hello, World!\nHello, World!")
+# print("Hello," + " Naseebullah")
+# print("Hello " + input("What is your name?") + "!")
+
+# name = input("What is your name?")
+# print(name)
+
+# name = "Jack"
+# print(name)
+# name = "Angela"
+# print(name)
+
+# print(len(input("What is your name?")))
+# name = input("What is your name?")
+# length = len(name)
+# print(length)
+
+# name convention
+# n = input("What is your name?")
+# l = len(n)
+# print(l)
+
+user_name = input("Wha is your name?")
+length = len(user_name)
+print(length)
