c14820f0142fb85dafaf06de9ab561b0d385c1b6
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2024-01-22 21:28:36 +0430

    Day 15 Practice

--- main.py
+++ main.py
@@ -1,55 +1,89 @@
-import random
-from os import system as clear
-from art import logo, vs
-from game_data import data
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+money = 0
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
 
 
-def format_data(account):
-  account_name = account["name"]
-  account_description = account["description"]
-  account_country = account["country"]
-  return f"{account_name}, a {account_description}, from {account_country}."
+def check_resources(drink_ingredients):
+    """Returns True when order can be made, False if ingredients are insufficient."""
+    for ingredient in drink_ingredients:
+        if drink_ingredients[ingredient] >= resources[ingredient]:
+            print(f"Sorry there is not enough {ingredient}.")
+            return False
+    return True
 
 
-def check_answer(guess, account_a_follower_count, account_b_follower_count):
-  if account_a_follower_count > account_b_follower_count:
-    return guess == "a"
-  else:
-    return guess == "b"
+def process_coins():
+    """Returns the total calculated from coins inserted."""
+    print("Please insert Coins.")
+    total = int(input("How many quarters?: ")) * 0.25
+    total += int(input("How many dimes?: ")) * 0.1
+    total += int(input("How many nickles?: ")) * 0.05
+    total += int(input("How many pennies?: ")) * 0.01
+    return total
 
 
-print(logo)
+def check_transaction(money_received, drink_cost):
+    """Returns True when money is sufficient otherwise returns False."""
+    if money_received >= drink_cost:
+        cost = round(money_received - drink_cost, 2)
+        print(f"Here is ${cost} dollars in change")
+        global money
+        money += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
 
-count = 0
 
-account_a = random.choice(data)
-account_b = random.choice(data)
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from the resources."""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}. Enjoy!")
+
 
 while True:
-
-  account_a = account_b
-  account_b = random.choice(data)
-
-  while account_a == account_b:
-    account_b = random.choice(data)
-
-  print(f"Compare A: {format_data(account_a)}")
-  print(vs)
-  print(f"Against b: {format_data(account_b)}")
-
-  guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-  account_a_follower_count = account_a["follower_count"]
-  account_b_follower_count = account_b["follower_count"]
-
-  is_correct = check_answer(guess, account_a_follower_count,
-                            account_a_follower_count)
-
-  clear("cls")
-  print(logo)
-
-  if is_correct:
-    count += 1
-    print(f"You're right! Current score: {count}")
-  else:
-    print(f"Sorry, You're wrong, final score: {count}")
-    break
+    request = input("What would you like? (espresso/latte/cappuccino): ")
+    if request == "off":
+        break
+    elif request == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${money}")
+    else:
+        drink = MENU[request]
+        if check_resources(drink["ingredients"]):
+            payment = process_coins()
+            if check_transaction(payment, drink["cost"]):
+                make_coffee(request, drink["ingredients"])
