96c91fcc0107ae75a882fbd7f8bc5c6bb8fd9602
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2024-01-22 19:36:58 +0430

    Day 10 Practice

--- main.py
+++ main.py
@@ -1,62 +1,11 @@
-programming_dictionary = {
-  "Bug":"An error in a program that prevents the program from unning as expected.", 
-  "Function":"A piece of code that you can easily call over and over again.",
-}
+# Return as an early exit
+def format_name(fName, lName):
+  """Take a first and last name and format it to return the title case version of the name."""
+  if fName == "" or lName == "":
+    return "You didn't provide valid value"
+  return f"{fName} {lName}".title()
 
-# Retrieving items from dictionary.
-# print(programming_dictionary["Function"])
-
-# Adding new items to dictionary
-programming_dictionary["Loop"] = "The action of doing something over and over again."
-
-# print(programming_dictionary)
-
-# Create an empty dictionary
-empty_dictionary = []
-
-# # Wipe an existing dictionary
-# programming_dictionary = {}
-# print(programming_dictionary)
-
-# Edit an item in a dictionary
-programming_dictionary["Bug"] = "A moth in your computer."
-# print(programming_dictionary)
-
-# loop through a dictionary
-for key in programming_dictionary:
-  print(key)
-  print(programming_dictionary[key])
-
-#########################################
-# Nesting
-capitals = {
-  "France": "Paris",
-  "Germany": "Berlin",
-}
-
-# Nesting a List in a Dictionary
-
-travel_log = {
-  "France": ["Paris", "Lille", "Dijon"],
-  "Germany": ["Berlin", "Hamburg", "Stuttgart"],
-}
-
-# Nesting Dictionary in a Dictionary
-travel_log = {
-  "France":{"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
-  "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
-}
-
-# Nesting Dictionary in a List
-travel_log = [
-  {
-    "Country": "France",
-    "cities_visited": ["Paris", "Lille", "Dijon"],
-    "total_visits": 12
-  },
-  {
-    "Country": "Germany",
-    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
-    "total_visits": 5
-  }
-]
+# Already used Function with Outputs
+length = len(format_name("naseeb", "khan"))
+print(length)
+print(format_name("naseeb", "khan"))
