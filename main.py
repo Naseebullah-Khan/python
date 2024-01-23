55fbaf1e70b2947e7c0b17bb1fabe68ebcaf9782
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2024-01-23 18:56:25 +0430

    Day 18 Practice

--- main.py
+++ main.py
@@ -1,28 +1,58 @@
-# Write your code below this line 👇
-# print("Hello, World!")
-# input("What is your name?")
-# print("Hello, World!\nHello, World!")
-# print("Hello," + " Naseebullah")
-# print("Hello " + input("What is your name?") + "!")
+from random import randint, choice
+from turtle import Turtle, Screen, colormode
 
-# name = input("What is your name?")
-# print(name)
+# def draw(move_forward, angle):
+#     tim.forward(move_forward)
+#     tim.right(angle)
 
-# name = "Jack"
-# print(name)
-# name = "Angela"
-# print(name)
+def random_color():
+    colormode(255)
+    red = randint(0, 255)
+    green = randint(0, 255)
+    blue = randint(0, 255)
+    color = (red, green, blue)
+    return color
 
-# print(len(input("What is your name?")))
-# name = input("What is your name?")
-# length = len(name)
-# print(length)
+tim = Turtle()
 
-# name convention
-# n = input("What is your name?")
-# l = len(n)
-# print(l)
 
-user_name = input("Wha is your name?")
-length = len(user_name)
-print(length)
+tim.shape("turtle")
+tim.color("red", "blue")
+
+
+# for _ in range(10):
+#     tim.forward(5)
+#     tim.penup()
+#     tim.forward(5)
+#     tim.pendown()
+# for num in range(3, 11):
+#     tim.color(random_color())
+#     for _ in range(num):
+#         draw(100, 360/num)
+
+
+
+
+
+# def random_walk(forward, angle):
+#     tim.color(random_color())
+#     tim.speed(randint(0, 11))
+#     tim.forward(forward)
+#     tim.setheading(angle)
+
+
+# direction = [0, 90, 180, 270]
+# for _ in range(200):
+#     tim.pensize(10)
+#     random_walk(40, choice(direction))
+
+tim.speed("fastest")
+step = 5
+for num in range(int(360 / step)):
+    print(num)
+    tim.color(random_color())
+    tim.circle(100)
+    tim.setheading(tim.heading() + step)
+
+screen = Screen()
+screen.exitonclick()
