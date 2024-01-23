b280456ea22f11c977ec8f1eff59e05111faffa7
Naseebullah Hoshmand <110621136+Naseebullah-Khan@users.noreply.github.com>
2024-01-23 22:41:38 +0430

    Day 20 and 21 Practice

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-from turtle import Turtle, Screen
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+
+    def breath(self):
+        print("inhale, exhale")
 
 
-def move():
-    turtle.forward(10)
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def breath(self):
+        super().breath()
+        print("Underwater")
+
+    def swim(self):
+        print("Swimming")
 
 
-turtle = Turtle()
-screen = Screen()
+nemo = Fish()
+nemo.swim()
+print(nemo.num_eyes)
+nemo.breath()
 
-screen.listen()
-screen.onkey(key="space", fun=move)
-screen.exitonclick()
+list_1 = ["a", "b", "c", "d", "f", "g", "h"]
+tuple_1 = ["do", "re", "mi", "fa", "so", "la", "ti"]
+print(list_1[3:6])
+print(list_1[3:])
+print(list_1[:6])
+print(list_1[::3])
+print(list_1[::-1])
+
+print(tuple_1[3:6])
+print(tuple_1[3:])
+print(tuple_1[:6])
+print(tuple_1[::3])
+print(tuple_1[::-1])
